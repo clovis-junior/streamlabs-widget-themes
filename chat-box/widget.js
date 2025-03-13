6444bdd936516eeba5edda9be88d249436a9db20
Clovis Junior <clovis.sa.junior@gmail.com>
2025-03-13 10:07:53 -0300

    Update widget.js

--- chat-box/widget.js
+++ chat-box/widget.js
@@ -1,14 +1,7 @@
-// Please use event listeners to run functions.
 document.addEventListener('onLoad', function(obj) {
 	// obj will be empty for chat widget
-	// this will fire only once when the widget loads
-});
-
+}),
 document.addEventListener('onEventReceived', function(obj) {
   	// obj will contain information about the event
-		/*if(obj.detail.command === 'PRIVMSG') {
-      document.querySelectorAll('.chat-user-badges').forEach(function(element, index){
-        element.style.setProperty('--badges', obj.detail.tags.badges.split(',').length)
-      })
-    }*/
-});
+	// if (isEmpty(obj)) return;
+})
